56f93f2ae0c468c1dd78db23e79b75cdd40233e2
mdnelles <mxnelles@gmail.com>
2021-07-19 21:20:53 -0400

    update routes

--- src/pages/auth/Epoch.js
+++ src/pages/auth/Epoch.js
@@ -5,6 +5,7 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { useEpochData } from "../../common/state/epoch/hooks";
 
 const useStyles = makeStyles((theme) => ({
    root: {
@@ -24,41 +25,61 @@ const useStyles = makeStyles((theme) => ({
    main: {
       minHeight: window.innerHeight,
    },
+   span1: {
+      color: "yellow",
+      display: "inline-block",
+   },
 }));
 
 const Epoch = (props) => {
    const classes = useStyles();
-   const [expanded, setExpanded] = React.useState(false);
+   const { epochData } = useEpochData();
 
-   const handleChange = (panel) => (event, isExpanded) => {
-      setExpanded(isExpanded ? panel : false);
+   const Activity = (props) => {
+      const { val, intensity } = props;
+      return (
+         <div className={classes.span1}>
+            {val} - {intensity}
+         </div>
+      );
    };
+
+   let objs = epochData;
+   objs.sort((a, b) => a.start_time.localeCompare(b.start_time));
+
+   React.useEffect(() => {}, [objs]);
+
    return (
       <div className={classes.main}>
          <h4>Epoch</h4>
-         <Accordion
-            expanded={expanded === "panel1"}
-            onChange={handleChange("panel1")}
-         >
-            <AccordionSummary
-               expandIcon={<ExpandMoreIcon />}
-               aria-controls='panel1bh-content'
-               id='panel1bh-header'
-            >
-               <Typography className={classes.heading}>
-                  General settings
-               </Typography>
-               <Typography className={classes.secondaryHeading}>
-                  I am an accordion
-               </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-               <Typography>
-                  Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
-                  feugiat. Aliquam eget maximus est, id dignissim quam.
-               </Typography>
-            </AccordionDetails>
-         </Accordion>
+         {objs.map((o, index) => (
+            <Accordion>
+               <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls='panel1a-content'
+                  id='panel1a-header'
+               >
+                  <Typography className={classes.heading}>
+                     {`${o.start_time} - ${o.end_time} `}
+                     <Activity val={o.activity_type} intensity={o.intensity} />
+                  </Typography>
+               </AccordionSummary>
+               <AccordionDetails>
+                  <Typography>
+                     <ul>
+                        <li>{o.active_calories} calories</li>
+                        <li>{o.active_time} (s)</li>
+                        <li>{o.distance} distance</li>
+                        <li>{o.duration} duration</li>
+                        <li>{o.max_motion_intensity} max motion intensity</li>
+                        <li>{o.mean_motion_intensity} mean motion intensity</li>
+                        <li>{o.met} met</li>
+                        <li>{o.steps} steps</li>
+                     </ul>
+                  </Typography>
+               </AccordionDetails>
+            </Accordion>
+         ))}
       </div>
    );
 };
